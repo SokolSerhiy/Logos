abbc2b51634816f133444c9f2f40ccbaf8889f25
Serhiy Sokol <sokol.serhij@gmail.com>
2015-08-25 12:45:13 +0300

    Full project

---
+++ LessonHibernateXML/src/lv/lgs/hibernate/domain/Book.java
@@ -0,0 +1,39 @@
+package lv.lgs.hibernate.domain;
+
+public class Book {
+	
+	private int id;
+	private String name;
+	private Student student;
+	
+	public Book() {
+	}
+
+	public Book(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+}
